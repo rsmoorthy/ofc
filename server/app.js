c04eb763a2f48fc290c01073870997effc05ed26
Moorthy RS <rsmoorthy@gmail.com>
2019-02-25 10:37:32 +0530

    set 3001 as default port

--- server/app.js
+++ server/app.js
@@ -23,6 +23,7 @@ var app = express()
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
+app.set('port', process.env.PORT || 3001)
 
 app.use(logger('dev'))
 app.use(express.json({ limit: '10mb' }))
